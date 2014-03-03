8e178714bf659653b619f3a4279d7913fe87c826
Jorge Angarita <jorgea@coldarmy-lm.corp.yahoo.com>
2014-03-03 14:54:44 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Jorge Angarita on 3/3/14.
+//  Copyright (c) 2014 jorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
